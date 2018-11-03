ac3e3ca24b7a674427be01216abd2987ba8ba46c
Muhammad Bilal Jawwad <mbj786.bilal@gmail.com>
2018-11-03 17:57:21 +0500

    Added editorConfig and eslinting support

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss';
 
-console.log('process.env.VERSION', process.env.VERSION);
-console.log('process.env.PLATFORM', process.env.PLATFORM);
-console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 const App = () => {
-    return (
-        <div>Hello World</div>
-    )
-}
+  return <div>Hello World</div>;
+};
 
 ReactDOM.render(<App />, document.querySelector('#app'));
